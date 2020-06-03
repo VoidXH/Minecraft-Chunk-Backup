d3f0511699ba4b9386f90ec9cd4411a2b724ad2d
VoidX <voidxb@gmail.com>
2020-06-03 21:15:49 +0200

    Skip duplicate regions

--- MinecraftChunkBackup/MainWindow.xaml.cs
+++ MinecraftChunkBackup/MainWindow.xaml.cs
@@ -14,15 +14,45 @@ namespace MinecraftChunkBackup {
         void AddWorldButton(object sender, RoutedEventArgs e) {
         }
 
+        /// <summary>Checks if a position is already in the list of regions.</summary>
+        bool HasRegion(int x, int z) {
+            for (int i = 0, end = regions.Count; i < end; ++i) {
+                Position pos = regions[i].Region.Pos;
+                if (pos.X < x)
+                    continue;
+                if (pos.X > x)
+                    return false;
+                if (pos.Z == z)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>Adds a region and keeps <see cref="regions"/> sorted.</summary>
+        void AddSortedRegion(int x, int z) {
+            int pos = 0, end = regions.Count;
+            for (; pos < end; ++pos) {
+                Position coords = regions[pos].Region.Pos;
+                if (coords.X < x)
+                    continue;
+                if (coords.X > x || coords.Z > z)
+                    break;
+            }
+            regions.Add(new RegionEntry(new Region(x, z)));
+            if (pos != end)
+                regions.Move(end, pos);
+        }
+
+        /// <summary>Show an <see cref="AddRegions"/> dialog and add the results that are not already on the <see cref="regions"/> list.</summary>
         void AddRegionsButton(object sender, RoutedEventArgs e) {
-            // TODO: skip duplicates
             AddRegions adder = new AddRegions();
             if (adder.ShowDialog().Value)
                 for (int x = adder.regionStartX.Value, xEnd = adder.regionEndX.Value, xDir = Math.Sign(xEnd - x + .01);
                     x != xEnd + xDir; x += xDir)
                     for (int z = adder.regionStartZ.Value, zEnd = adder.regionEndZ.Value, zDir = Math.Sign(zEnd - z + .01);
                         z != zEnd + zDir; z += zDir)
-                        regions.Add(new RegionEntry(new Region(x, z)));
+                        if (!HasRegion(x, z))
+                            AddSortedRegion(x, z);
         }
     }
 }
